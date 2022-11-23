659c60a4b8c8a3542a25662099aba68a85d1affe
Shayan <88369606+shayan55579@users.noreply.github.com>
2022-11-23 11:12:54 +0330

    Add files via upload

---
+++ XmlDataSetToYolo/XmlDataSetToYolo/Datum.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Xml2CSharp
+{
+    [XmlRoot(ElementName = "bndbox")]
+    public class BoundingBox
+    {
+        [XmlElement(ElementName = "xmin")]
+        public float Xmin { get; set; }
+        [XmlElement(ElementName = "ymin")]
+        public float Ymin { get; set; }
+        [XmlElement(ElementName = "xmax")]
+        public float Xmax { get; set; }
+        [XmlElement(ElementName = "ymax")]
+        public float Ymax { get; set; }
+        
+        public float X
+        {
+            get
+            {
+                return Xmin;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return Ymin;
+            }
+        }
+        public float Width
+        {
+            get
+            {
+                // compute width from xmin and xmax
+                return Xmax - Xmin;
+            }
+        }
+        public float Height
+        {
+            get
+            {
+                return Ymax - Ymin;
+            }
+        }
+
+        public RectangleF Rect
+        {
+            get
+            {
+                return new RectangleF(Xmin, Ymin, Width, Height);
+            }
+        }
+    }
+
+    [XmlRoot(ElementName = "object")]
+    public class Object
+    {
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "bndbox")]
+        public BoundingBox BoundingBox { get; set; }
+    }
+
+    [XmlRoot(ElementName = "annotation")]
+    public class Annotation
+    {
+        [XmlElement(ElementName = "filename")]
+        public List<string> Filename { get; set; }
+        [XmlElement(ElementName = "object")]
+        public List<Object> Object { get; set; }
+        [XmlElement(ElementName = "folder")]
+        public string Folder { get; set; }
+    }
+
+}
---
+++ XmlDataSetToYolo/XmlDataSetToYolo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml;
+using System.Xml.Serialization;
+
+using Xml2CSharp;
+
+var folderAddress = Path.Combine(args[0], "Letters");
+Console.WriteLine(folderAddress);
+Directory.CreateDirectory(folderAddress);
+var counter = 0;
+
+foreach (var file in Directory.EnumerateFiles(args[0], "*.xml"))
+{
+    Console.WriteLine(file);
+
+    var serializer = new XmlSerializer(typeof(Annotation));
+    using var xmlReader = XmlReader.Create(file);
+    var annotaion = (Annotation)serializer.Deserialize(xmlReader);
+
+    var elements = annotaion.Object;
+    var imageFileName = file.Replace(".xml", ".jpg");
+    
+    
+    var image = (Bitmap)Image.FromFile(imageFileName);
+    
+    foreach (var item in elements)
+    {
+        try
+        {
+            var letterFile = image.Clone(item.BoundingBox.Rect, image.PixelFormat);
+            var folderAddressForLetter = Path.Combine(folderAddress, item.Name);
+            Directory.CreateDirectory(folderAddressForLetter);
+            var fileName = Path.Combine(folderAddressForLetter, $"{counter:D6}.png");
+            
+            letterFile.Save(fileName, ImageFormat.Png);
+
+            counter++;
+        }
+        catch
+        {
+
+        }
+        
+    }
+}
